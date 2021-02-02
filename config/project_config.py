7d07b1e9be83c8673d8483467b4d90fb6b3770b3
Hamza Usman Ghani <51050375+hamzausmanghani@users.noreply.github.com>
2021-02-02 21:21:44 +0500

    Login API Completed

--- config/project_config.py
+++ config/project_config.py
@@ -1,7 +1,7 @@
 config = {
     # config for database
     'DB_USERNAME': 'root',
-    'DB_PASSWORD': '0900',
+    'DB_PASSWORD': 'root',
     'DB_HOST': 'localhost',
-    'DB_NAME': 'info_influencer_test1',
+    'DB_NAME': 'ckbsm',
 }
