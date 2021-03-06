410343c254774c960d87fe7c6ddeca9913b37858
Muhammad Safwan <s.aryani456@gmail.com>
2021-03-06 18:57:06 +0500

    user views done

--- common_apis/v1/user_views.py
+++ common_apis/v1/user_views.py
@@ -1,32 +1,27 @@
-from flask import request, jsonify, Blueprint
+from flask import Blueprint
 from helper.authorization import authorize_request
 from helper.request_response import *
 from helper.validate import *
-from helper.database import *
 from constants.route_constants import *
 from constants.flask_constants import *
-from constants.table_names import *
 from constants.column_names import *
-from database_layer.database import select_query, insert_query
 from codes.status_codes import *
 from codes.response_codes import *
-from config.db_column_to_response import mapper
 from helper.user import find_user_by_id, enroll_user_in_madrassa
 from helper.madrassa_detail import find_madrassa_detail_by_id
 from helper.role import find_role_by_id
+from helper.request_response import requires
+from helper.user import add_new_user, find_user_by_cnic_or_contact
 
 user_api = Blueprint("user_api", __name__, url_prefix='')
 
 
 @user_api.route(ADD_USER, methods=[POST])
 @authorize_request
+@requires([gc.NAME, gc.FATHER_NAME, gc.CNIC, gc.EMAIL, gc.MOTHER_TONGUE, gc.CONTACT, gc.GENDER_ID, gc.GUARDIAN_NAME,
+           gc.GUARDIAN_CONTACT, gc.DATE_OF_BIRTH, gc.AGE], body=gc.JSON)
 def add_user():
     request_body = request.get_json()
-    missing = verify_param([gc.NAME, gc.FATHER_NAME, gc.CNIC, gc.EMAIL, gc.MOTHER_TONGUE, gc.CONTACT, gc.GENDER_ID,
-                            gc.GUARDIAN_NAME, gc.GUARDIAN_CONTACT, gc.DATE_OF_BIRTH, gc.AGE], request_body)
-    if missing:
-        response = make_general_response(PARAMETER_MISSING, missing + " is missing")
-        return response, BAD_REQUEST
 
     if not validate_cnic(request_body.get(gc.CNIC)):
         response = make_general_response(INVALID_PARAMETER, "Invalid CNIC")
@@ -48,120 +43,50 @@ def add_user():
         response = make_general_response(INVALID_PARAMETER, "Invalid DOB. Allowed Date format is YYYYMMDD")
         return response, BAD_REQUEST
 
-    name = request_body.get(gc.NAME)
-    fname = request_body.get(gc.FATHER_NAME)
-    cnic = request_body.get(gc.CNIC)
-    email = request_body.get(gc.EMAIL)
-    mt = request_body.get(gc.MOTHER_TONGUE)
-    contact = request_body.get(gc.CONTACT)
-    gender_id = request_body.get(gc.GENDER_ID)
-    guardian_name = request_body.get(gc.GUARDIAN_NAME)
-    guardian_contact = request_body.get(gc.GUARDIAN_CONTACT)
-    dob = request_body.get(gc.DATE_OF_BIRTH)
-    age = request_body.get(gc.AGE)
+    user_param = request_body
 
-    query = f"select * from {GENDER} where {ID}={gender_id}"
-    r = select_query(query)
-    result = r.fetchall()
-    print(result)
-    if len(result) == 0:
-        response = make_general_response(GENDER_NOT_FOUND, f"GenderID {gender_id} doesn't exists")
-        return response, BAD_REQUEST
-
-    index = select_max(USER) + 1
-    query = f"insert into {USER}({ID},{USER_NAME},{FATHER_NAME},{CNIC},{EMAIL},{MOTHER_TONGUE},{CONTACT}," \
-            f"{GENDER_ID},{GUARDIAN_NAME},{GUARDIAN_CONTACT},{DOB},{AGE})" \
-            f"values({index},'{name}','{fname}','{cnic}','{email}','{mt}','{contact}',{gender_id}," \
-            f"'{guardian_name}','{guardian_contact}','{dob}','{age}')"
-    print(query)
-    r = insert_query(query)
-    if r:
-        query = f"select * from {USER} where {ID} = {index}"
-        r = select_query(query)
-        result = r.fetchall()
-        for i in result:
-            data = map_response(i, mapper)
-        # data = {
-        #     gc.ID: result[0][0],
-        #     gc.NAME: result[0][1],
-        #     gc.FATHER_NAME: result[0][2],
-        #     gc.CNIC: result[0][3],
-        #     gc.EMAIL: result[0][4],
-        #     gc.MOTHER_TONGUE: result[0][5],
-        #     gc.CONTACT: result[0][6],
-        #     gc.GENDER_ID: result[0][7],
-        #     gc.GUARDIAN_NAME: result[0][8],
-        #     gc.GUARDIAN_CONTACT: result[0][9],
-        #     gc.DATE_OF_BIRTH: result[0][10],
-        #     gc.AGE: result[0][11],
-        # }
-        response = make_general_response(SUCCESS, "SUCCESS")
+    data, response_code, detail = add_new_user(user_param)
+    response = make_general_response(response_code, detail)
+    if data:
         response[gc.DATA] = data
         return response, CREATED
-
-    else:
-        response = make_general_response(FAIL, "FAIL")
-        return response, BAD_REQUEST
+    return response, BAD_REQUEST
 
 
 @user_api.route(GET_USERS, methods=[GET])
 @authorize_request
 def get_users():
     query_params = request.args
-    length_query_param = len(query_params)
-    if length_query_param == 0:
-        response = make_general_response(MISSING_QUERY_PARAM, "Query params are missing")
-        return response, BAD_REQUEST
-
-    elif not query_params.get(CNIC) and not query_params.get(CONTACT):
+    if not query_params.get(CNIC) and not query_params.get(CONTACT):
         response = make_general_response(INVALID_QUERY_PARAM, "Invalid query params")
         return response, BAD_REQUEST
 
     cnic = query_params.get(CNIC)
     contact = query_params.get(CONTACT)
 
-    where_clause = ""
+    if cnic and not validate_cnic(cnic):
+        response = make_general_response(INVALID_PARAMETER, "Invalid CNIC")
+        return response, BAD_REQUEST
 
-    if cnic or contact:
-        where_clause = where_clause + "where"
+    if contact and not validate_contact(contact):
+        response = make_general_response(INVALID_PARAMETER, "Invalid contact")
+        return response, BAD_REQUEST
 
-    if cnic:
-        if not validate_cnic(cnic):
-            response = make_general_response(INVALID_PARAMETER, "Invalid CNIC")
-            return response, BAD_REQUEST
-        where_clause = where_clause + f" {CNIC}='{cnic}' "
-        if contact:
-            where_clause = where_clause + "and"
-
-    if contact:
-        if not validate_contact(contact):
-            response = make_general_response(INVALID_PARAMETER, "Invalid Contact")
-            return response, BAD_REQUEST
-        where_clause = where_clause + f" ({CONTACT}='{contact}' or {GUARDIAN_CONTACT}='{contact}')"
-
-    query = f"select * from {USER} " + where_clause
-    print(query)
-    r = select_query(query)
-    result = r.fetchall()
-    data = []
-    for i in result:
-        data.append(map_response(i, mapper))
-
-    response = make_general_response(SUCCESS, "Success")
-    response[gc.DATA] = data
-
-    return response, OK
+    users = find_user_by_cnic_or_contact(cnic, contact)
+    if users:
+        response = make_general_response(SUCCESS, "Success")
+        response[gc.DATA] = users
+        return response, OK
+    else:
+        response = make_general_response(USER_NOT_FOUND, "User Not found")
+        return response, BAD_REQUEST
 
 
 @user_api.route(ENROLL_USER, methods=[POST])
 @authorize_request
+@requires([gc.USER_ID, gc.ENROLMENT_DATE, gc.MADRASSA_DETAIL, gc.ROLE_ID], body=gc.JSON)
 def enroll_users():
     request_body = request.get_json()
-    missing = verify_param([gc.USER_ID, gc.ENROLMENT_DATE, gc.MADRASSA_DETAIL, gc.ROLE_ID], request_body)
-    if missing:
-        response = make_general_response(PARAMETER_MISSING, missing + " is missing")
-        return response, BAD_REQUEST
-
     _id = request_body[gc.USER_ID]
     user = find_user_by_id(_id)
 
@@ -188,11 +113,8 @@ def enroll_users():
         response = make_general_response(INVALID_PARAMETER, "Invalid Date Format. Allowed Date format is YYYYMMDD")
         return response, BAD_REQUEST
 
-    enrolled, code = enroll_user_in_madrassa(user, madrassa_detail, role, enrollment_date)
+    enrolled, response_code, detail = enroll_user_in_madrassa(user, madrassa_detail, role, enrollment_date)
+    response = make_general_response(response_code, detail)
     if enrolled:
-        response = make_general_response(code,"SUCCESS")
         return response, OK
-    response = make_general_response(code, "FAIL")
     return response, BAD_REQUEST
-
-    return jsonify({request.base_url: request.method})
--- helper/user.py
+++ helper/user.py
@@ -1,11 +1,14 @@
 from constants.table_names import USER, ENROLLMENT
-from constants.column_names import ID, USER_ID, ROLE_ID, MADRASSA_DETAIL_ID, ENROLLMENT_DATE, GENDER_ID
+from constants.column_names import ID, USER_ID, ROLE_ID, MADRASSA_DETAIL_ID, ENROLLMENT_DATE, GENDER_ID, USER_NAME, \
+    FATHER_NAME, CNIC, EMAIL, MOTHER_TONGUE, CONTACT, GUARDIAN_NAME, GUARDIAN_CONTACT, DOB, AGE
+from codes.response_codes import GENDER_NOT_FOUND
 import constants.general_constants as gc
 from config.db_column_to_response import mapper
 from database_layer.database import select_query, insert_query
 from helper.request_response import map_response
 from helper.database import select_max
 from codes.response_codes import USER_ALREADY_ENROLLED, SUCCESS, FAIL
+from database_layer import database
 
 
 def user_already_enrolled(user, madrassa_detail, role):
@@ -50,21 +53,6 @@ def find_user_by_gender(gender_id):
     return None
 
 
-def enroll_user_in_madrassa(user, madrassa_detail, role, enrollment_date):
-    if user_already_enrolled(user, madrassa_detail, role):
-        return False, USER_ALREADY_ENROLLED
-
-    _id = select_max(ENROLLMENT) + 1
-
-    query = f"insert into {ENROLLMENT}({ID},{ENROLLMENT_DATE},{USER_ID},{ROLE_ID},{MADRASSA_DETAIL_ID}) " \
-            f"values({_id},'{enrollment_date}',{user[gc.ID]},{role[gc.ID]},{madrassa_detail[gc.ID]})"
-    print(query)
-    inserted = insert_query(query)
-    if inserted:
-        return True, SUCCESS
-    return False, FAIL
-
-
 def find_enrollment_of_role(role_id):
     query = f"select * from {ENROLLMENT} where {ROLE_ID}={role_id}"
     r = select_query(query)
@@ -75,3 +63,76 @@ def find_enrollment_of_role(role_id):
             enrollment = map_response(i, mapper)
         return enrollment
     return None
+
+
+def find_user_by_cnic_or_contact(cnic, contact):
+    where_clause = ""
+
+    if cnic or contact:
+        where_clause = where_clause + "where"
+    if cnic:
+        where_clause = where_clause + f" {CNIC}='{cnic}' "
+        if contact:
+            where_clause = where_clause + "and"
+    if contact:
+        where_clause = where_clause + f" ({CONTACT}='{contact}' or {GUARDIAN_CONTACT}='{contact}')"
+    query = f"select * from {USER} " + where_clause
+    print(query)
+    r = database.select_query(query)
+    if r:
+        result = r.fetchall()
+        data = []
+        for i in result:
+            data.append(map_response(i, mapper))
+        return data
+    return None
+
+
+def enroll_user_in_madrassa_by_id(_id, user, madrassa_detail, role, enrollment_date):
+    query = f"insert into {ENROLLMENT}({ID},{ENROLLMENT_DATE},{USER_ID},{ROLE_ID},{MADRASSA_DETAIL_ID}) " \
+            f"values({_id},'{enrollment_date}',{user[gc.ID]},{role[gc.ID]},{madrassa_detail[gc.ID]})"
+    return database.insert_query(query)
+
+
+def enroll_user_in_madrassa(user, madrassa_detail, role, enrollment_date):
+    if user_already_enrolled(user, madrassa_detail, role):
+        return False, USER_ALREADY_ENROLLED, "User Already enrolled"
+
+    max_index = select_max(ENROLLMENT)
+    if max_index is None:
+        return [], FAIL, "FAIL"
+
+    index = select_max(ENROLLMENT) + 1
+    r = enroll_user_in_madrassa_by_id(index, user, madrassa_detail, role, enrollment_date)
+    if r:
+        return r, SUCCESS, "Success"
+    return r, FAIL, "Fail"
+
+
+def insert_user_by_id(_id, user):
+    query = f"insert into {USER}({ID},{USER_NAME},{FATHER_NAME},{CNIC},{EMAIL},{MOTHER_TONGUE},{CONTACT}," \
+            f"{GENDER_ID},{GUARDIAN_NAME},{GUARDIAN_CONTACT},{DOB},{AGE})" \
+            f"values({_id},'{user[gc.NAME]}','{user[gc.FATHER_NAME]}','{user[gc.CNIC]}','{user[gc.EMAIL]}'," \
+            f"'{user[gc.MOTHER_TONGUE]}','{user[gc.CONTACT]}',{user[gc.GENDER_ID]}," \
+            f"'{user[gc.GUARDIAN_NAME]}','{user[gc.GUARDIAN_CONTACT]}','{user[gc.DATE_OF_BIRTH]}','{user[gc.AGE]}')"
+    return database.insert_query(query)
+
+
+def add_new_user(user):
+    # importing here due to circular import error
+
+    from helper.gender import find_gender_by_id
+    gender = find_gender_by_id(user[gc.GENDER_ID])
+    if gender is None:
+        return [], GENDER_NOT_FOUND, "gender Not Found"
+
+    max_index = select_max(USER)
+    if max_index is None:
+        return [], FAIL, "FAIL"
+
+    index = max_index + 1
+    r = insert_user_by_id(index, user)
+    if r:
+        user = find_user_by_id(index)
+        return user, SUCCESS, "SUCCESS"
+    return [], FAIL, "FAIL"
