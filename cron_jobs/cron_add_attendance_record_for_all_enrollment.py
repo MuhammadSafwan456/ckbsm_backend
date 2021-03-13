a252061ccec400e4941be5ab1f15672d792680a0
Muhammad Safwan <s.aryani456@gmail.com>
2021-03-13 15:49:58 +0500

    cron job run explicitly

--- cron_jobs/cron_add_attendance_record_for_all_enrollment.py
+++ cron_jobs/cron_add_attendance_record_for_all_enrollment.py
@@ -19,3 +19,6 @@ def cron_add_attendance_record_for_all_enrollments():
         if attendance is None:
             create_attendance_for_enrollment_at_date(enrollment[gc.ID], date)
 
+
+if __name__ == '__main__':
+    cron_add_attendance_record_for_all_enrollments()
