7503766a0f870824c9da5792c546b31273a5c72e
Muhammad Safwan <s.aryani456@gmail.com>
2021-03-13 22:23:33 +0500

    bulk user attendance

--- common_apis/v1/attendance_views.py
+++ common_apis/v1/attendance_views.py
@@ -47,11 +47,58 @@ def user_attendance():
     return response, BAD_REQUEST
 
 
-
 @attendance_api.route(BULK_USER_ATTENDANCE, methods=[POST])
 @authorize_request
+@requires([gc.ENROLLMENT_IDS, gc.DATE, gc.STATUS], body=gc.JSON)
 def bulk_user_attendance():
-    return jsonify({request.base_url: request.method})
+    request_body = request.get_json()
+    enrollment_ids = request_body[gc.ENROLLMENT_IDS]
+    date = request_body[gc.DATE]
+    status_id = request_body[gc.STATUS]
+
+    if not validate_date(date):
+        response = make_general_response(INVALID_PARAMETER, "Invalid Date Format. Allowed Date format is YYYYMMDD")
+        return response, BAD_REQUEST
+
+    status = find_attendance_status_by_id(status_id)
+    if not status:
+        response = make_general_response(ATTENDANCE_STATUS_NOT_FOUND, "Attendance Status not found")
+        return response, BAD_REQUEST
+
+    if not isinstance(enrollment_ids, list):
+        response = make_general_response(ATTENDANCE_STATUS_NOT_FOUND, "Attendance Status not found")
+        return response, BAD_REQUEST
+    marked_list = []
+    unmark_list = []
+    already_marked = []
+    enrollment_not_found = []
+    for enrollment_id in enrollment_ids:
+        enrollment = find_enrollment_by_id(enrollment_id)
+        if not enrollment:
+            enrollment_not_found.append(enrollment_id)
+            continue
+
+        marked, code, _ = mark_attendance(enrollment_id, date, status_id)
+        if marked:
+            marked_list.append(enrollment_id)
+        elif code==ATTENDANCE_ALREADY_MARKED:
+            already_marked.append(enrollment_id)
+        else:
+            unmark_list.append(enrollment_id)
+
+    response = make_general_response(SUCCESS,"SUCCESS")
+    response["marked"] = marked_list
+    response['unmarked'] = unmark_list
+    response["enrollmentNotFound"] = enrollment_not_found
+    response["alreadyMarked"] = already_marked
+    return response,OK
+
+
+
+
+
+
+
 
 
 @attendance_api.route(GET_ALL_ATTENDANCE, methods=[GET])
